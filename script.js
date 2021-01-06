8749643ec61a9a3937223a7814b03b9e537afdb4
Dhiraj3118 <61940283+Dhiraj3118@users.noreply.github.com>
2021-01-06 08:21:20 +0530

    Latest script.js added

---
+++ script.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector("#toggleNav");
+let element = document.querySelector("#toggleClass");
+btn.addEventListener("click", () => {
+  element.classList.toggle("hidden");
+});
