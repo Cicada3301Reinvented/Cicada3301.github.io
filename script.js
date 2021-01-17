ce696e0bed22fb9954d1676c80c638a95c6a974d
EFB <mynameisrone@gmail.com>
2021-01-18 01:34:06 +0530

    feat: sponsors, animations, team fix: timeline, contact form

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-let btn = document.querySelector("#toggleNav");
-let element = document.querySelector("#toggleClass");
-btn.addEventListener("click", () => {
-  element.classList.toggle("hidden");
-});
+let btn = document.querySelector('#toggleNav')
+let element = document.querySelector('#toggleClass')
+btn.addEventListener('click', () => {
+   element.classList.toggle('hidden')
+})
 
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+var pageContent = document.getElementById('content'),
+   pagecopy = pageContent.cloneNode(true),
+   blurryContent = document.getElementById('blurryscroll')
+blurryContent.appendChild(pagecopy)
+window.onscroll = function () {
+   blurryContent.scrollTop = window.pageYOffset
 }
-
-document.getElementById("defaultOpen").click();
